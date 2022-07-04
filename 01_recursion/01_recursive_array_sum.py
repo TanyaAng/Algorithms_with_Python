8b0808b19329fcde65fb694acaa94a0ceb1ae5dc
TanyaAng <t.j.angelova@gmail.com>
2022-07-04 19:18:36 +0300

    Lab_exercise

---
+++ 01_recursion/01_recursive_array_sum.py
@@ -0,0 +1,8 @@
+def array_sum(nums, idx=0):
+    if idx == len(nums) - 1:
+        return nums[idx]
+    return nums[idx] + array_sum(nums, idx + 1)
+
+
+nums = [int(x) for x in input().split()]
+print(array_sum(nums))
