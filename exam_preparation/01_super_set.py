a4ac94461afcd6c530a694b8764e1eaf0b4918f6
TanyaAng <t.j.angelova@gmail.com>
2022-07-10 18:07:56 +0300

    Lab_exercise

--- exam_preparation/01_super_set.py
+++ exam_preparation/01_super_set.py
@@ -1,12 +1,5 @@
 import numpy as np
 
-
-def is_unique_elements(subset):
-    if len(subset) == len(set(subset)):
-        return True
-    return False
-
-
 '''Not the appropriate solution'''
 # def find_super_set(idx, collection, subset):
 #     if is_unique_elements(subset):
@@ -29,19 +22,53 @@ def is_unique_elements(subset):
 '''Can not order by the requirments'''
 
 
-class py_solution:
-    def sub_sets(self, subset):
-        return sorted(self.__subsetsRecur([], subset), key=lambda a: (len(a)))
+# class py_solution:
+#     def sub_sets(self, subset):
+#         return sorted(self.__subsetsRecur([], subset), key=lambda a: (len(a)))
+#
+#     def __subsetsRecur(self, current, subset):
+#         if subset:
+#             item_1 = self.__subsetsRecur(current, subset[1:])
+#             item_2 = self.__subsetsRecur(current + [subset[0]], subset[1:])
+#             result = item_1 + item_2
+#             return result
+#         return [current]
+#
+#
+# collection = sorted([int(x) for x in input().split(', ')])
+# solution = py_solution()
+# print(solution.sub_sets(collection))
 
-    def __subsetsRecur(self, current, subset):
-        if subset:
-            item_1 = self.__subsetsRecur(current, subset[1:])
-            item_2 = self.__subsetsRecur(current + [subset[0]], subset[1:])
-            result = item_1 + item_2
-            return result
-        return [current]
+
+def is_lexicographical_order(array):
+    return sorted(array) == array
+
+
+def is_unique_elements(subset):
+    return len(subset) == len(set(subset))
+
+
+def clear_vector(vector):
+    return [item for item in vector if item != None]
+
+
+def super_set(idx, collection, vector):
+    if idx >= len(vector):
+        if is_unique_elements(vector) and is_lexicographical_order(vector):
+            print(*vector, sep=' ')
+        return
+    for i in range(len(collection)):
+        if collection[i] in vector:
+            continue
+        vector[idx] = collection[i]
+        cleared_vector = clear_vector(vector)
+        if len(cleared_vector) < len(vector) and is_unique_elements(cleared_vector) and is_lexicographical_order(
+                cleared_vector):
+            print(*cleared_vector, sep=' ')
+        super_set(idx + 1, collection, vector)
+        vector[idx] = None
 
 
 collection = sorted([int(x) for x in input().split(', ')])
-solution = py_solution()
-print(solution.sub_sets(collection))
+vector = [None] * len(collection)
+super_set(0, collection, vector)
