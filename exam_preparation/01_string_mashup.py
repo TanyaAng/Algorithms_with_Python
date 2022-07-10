df555a41749550312cf63b567a7dffdd7832dad7
TanyaAng <t.j.angelova@gmail.com>
2022-07-10 16:33:52 +0300

    Lab_exercise

--- exam_preparation/01_string_mashup.py
+++ exam_preparation/01_string_mashup.py
@@ -1,33 +1,23 @@
-# def string_mashup(idx, symbols, vector):
-#     result=[]
-#     if idx >= len(vector):
-#         result.append(vector)
-#         return
-#     for item in symbols:
-#         vector[idx] = item
-#         string_mashup(idx + 1, symbols, vector)
-#     return result
-#
-#
-# symbols = list(input())
-# n = int(input())
-# vector = [None] * n
-# print(string_mashup(0, symbols, vector))
+def is_lexicographical_order(array):
+    if sorted(array) == array:
+        return True
+    return False
 
-def find_nuclear(idx, collection, vector, used):
+
+def find_nuclear(idx, collection, vector):
     if idx >= len(vector):
-        print(*vector, sep='')
+        if is_lexicographical_order(vector):
+            print(*vector, sep='')
         return
     for i in range(len(collection)):
-        if not used[i]:
-            used[i] = True
-            vector[idx] = collection[i]
-            find_nuclear(idx + 1, collection, vector, used)
-            used[i] = False
+        vector[idx] = collection[i]
+        find_nuclear(idx + 1, collection, vector)
 
 
 symbols = sorted(list(input()))
 n = int(input())
 used = [False] * len(symbols)
 vector = [None] * n
-find_nuclear(0, symbols, vector, used)
+find_nuclear(0, symbols, vector)
+
+
--- exam_preparation/01_super_set.py
+++ exam_preparation/01_super_set.py
@@ -1,8 +1,5 @@
-# def find_super_set(collection):
-#     if collection == []:
-#         return [[]]
-#     x = find_super_set(collection[1:])
-#     return x + [[collection[0]] + y for y in x]
+import numpy as np
+
 
 def is_unique_elements(subset):
     if len(subset) == len(set(subset)):
@@ -10,27 +7,41 @@ def is_unique_elements(subset):
     return False
 
 
-def find_super_set(idx, collection, subset):
-    if is_unique_elements(subset):
-        print(*subset)
-    for i in range(idx, len(collection)):
-        subset.append(collection[i])
-        find_super_set(idx + 1, collection, subset)
-        subset.pop()
-    return
+'''Not the appropriate solution'''
+# def find_super_set(idx, collection, subset):
+#     if is_unique_elements(subset):
+#         print(*subset)
+#     for i in range(idx, len(collection)):
+#         subset.append(collection[i])
+#         find_super_set(idx + 1, collection, subset)
+#         subset.pop()
+#     return
+
+'''The same as class implementation'''
+# def find_super_set(current, collection):
+#     if collection:
+#         item_1 = find_super_set(current, collection[1:])
+#         item_2 = find_super_set(current + [collection[0]], collection[1:])
+#         result = item_1 + item_2
+#         return result
+#     return [current]
+
+'''Can not order by the requirments'''
 
 
 class py_solution:
     def sub_sets(self, subset):
-        return sorted(self.subsetsRecur([], subset), key=lambda a: (len(a)))
+        return sorted(self.__subsetsRecur([], subset), key=lambda a: (len(a)))
 
-    def subsetsRecur(self, current, subset):
+    def __subsetsRecur(self, current, subset):
         if subset:
-            item_1=self.subsetsRecur(current, subset[1:])
-            item_2=self.subsetsRecur(current + [subset[0]], subset[1:])
-            return item_1+item_2
+            item_1 = self.__subsetsRecur(current, subset[1:])
+            item_2 = self.__subsetsRecur(current + [subset[0]], subset[1:])
+            result = item_1 + item_2
+            return result
         return [current]
 
 
 collection = sorted([int(x) for x in input().split(', ')])
-print(py_solution().sub_sets(collection))
+solution = py_solution()
+print(solution.sub_sets(collection))
