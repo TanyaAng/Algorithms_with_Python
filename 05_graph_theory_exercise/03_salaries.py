2b7eddfe484d969e4cc1fef5d2522389b4fc80d1
TanyaAng <t.j.angelova@gmail.com>
2022-07-19 20:37:20 +0300

    Lab_exercise

--- 05_graph_theory_exercise/03_salaries.py
+++ 05_graph_theory_exercise/03_salaries.py
@@ -1,27 +1,62 @@
-def dfs(node, graph):
-    if graph[node] == []:
+# def dfs(node, graph):
+#     if graph[node] == []:
+#         return 1
+#     current_salaries = 0
+#     for child in graph[node]:
+#         current_salaries += dfs(child, graph)
+#     return current_salaries
+#
+#
+# n = int(input())
+# employee_matrix = []
+# for _ in range(n):
+#     employee_matrix.append(list(input()))
+#
+# graph = {}
+# for i in range(n):
+#     graph[i] = []
+#     for j in range(n):
+#         if employee_matrix[i][j] == 'Y':
+#             graph[i].append(j)
+#
+# total_salaries = 0
+#
+# for node in graph.keys():
+#     total_salaries += dfs(node, graph)
+#
+# print(f"{total_salaries}")
+
+
+# VARIANT 2
+def dfs(node, graph, salaries):
+    if salaries[node] is not None:
+        return salaries[node]
+    if len(graph[node]) == 0:
+        salaries[node] = 1
         return 1
-    current_salaries = 0
+    salary = 0
     for child in graph[node]:
-        current_salaries += dfs(child, graph)
-    return current_salaries
+        salary += dfs(child, graph, salaries)
+    salaries[node] = salary
+    return salary
 
 
 n = int(input())
-employee_matrix = []
+
+graph = []
 for _ in range(n):
-    employee_matrix.append(list(input()))
+    line = input()
+    children = []
+    for idx, state in enumerate(line):
+        if state == 'Y':
+            children.append(idx)
+    graph.append(children)
 
-graph = {}
-for i in range(n):
-    graph[i] = []
-    for j in range(n):
-        if employee_matrix[i][j] == 'Y':
-            graph[i].append(j)
+salaries = [None] * n
 
-total_salaries = 0
+result = 0
+for node in range(len(graph)):
+    salary = dfs(node, graph, salaries)
+    result += salary
 
-for node in graph.keys():
-    total_salaries += dfs(node, graph)
-
-print(f"{total_salaries}")
+print(result)
