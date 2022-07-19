e9753657f3b4b8f92eaeaa4e46603ecdd4db9c05
TanyaAng <t.j.angelova@gmail.com>
2022-07-19 20:20:49 +0300

    Lab_exercise

--- 05_graph_theory_exercise/03_salaries.py
+++ 05_graph_theory_exercise/03_salaries.py
@@ -1,17 +1,3 @@
-def find_dependencies(matrix):
-    result = {}
-    for i in range(len(matrix)):
-        if i not in result:
-            result[i] = 0
-        for j in range(len(matrix)):
-            if matrix[i][j] == 'Y':
-                if j not in result:
-                    result[j] = 1
-                else:
-                    result[j] += 1
-    return result
-
-
 def dfs(node, graph):
     if graph[node] == []:
         return 1
@@ -33,7 +19,6 @@ for i in range(n):
         if employee_matrix[i][j] == 'Y':
             graph[i].append(j)
 
-# dependencies = find_dependencies(employee_matrix)
 total_salaries = 0
 
 for node in graph.keys():
