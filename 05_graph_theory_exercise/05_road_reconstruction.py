0bc060f498e55f6f19593997d548a0f7eaea4ece
TanyaAng <t.j.angelova@gmail.com>
2022-07-19 21:15:23 +0300

    Lab_exercise

--- 05_graph_theory_exercise/05_road_reconstruction.py
+++ 05_graph_theory_exercise/05_road_reconstruction.py
@@ -1,55 +1,38 @@
-def find_dependencies(graph):
-    result = {}
-    for node, children in graph.items():
-        if node not in result:
-            result[node] = len(children)
-    return result
-
-
-def find_node_with_only_one_path(graph):
-    for node, dependencies in graph.items():
-        if dependencies == 1:
-            return node
-
-#
-# def sort_by_paths(graph):
-#     sorted_town = {}
-#     for key, value in sorted(graph.items(), key=lambda kvp: len(kvp[1])):
-#         sorted_town[key] = value
-#     return sorted_town
+def dfs(node, graph, visited):
+    if visited[node]:
+        return
+    visited[node] = True
+    for child in graph[node]:
+        dfs(child, graph, visited)
 
 
 buildings = int(input())
 streets = int(input())
 
-town = {}
+graph = []
+edges = set()
+
+[graph.append([]) for _ in range(buildings)]
+
 for _ in range(streets):
-    b1, b2 = input().split(' - ')
-    if b1 not in town:
-        town[b1] = []
-    if b2 not in town:
-        town[b2] = []
-    town[b1].append(b2)
-    town[b2].append(b1)
+    first, second = [int(x) for x in input().split(' - ')]
+    graph[first].append(second)
+    graph[second].append(first)
+    edges.add((min(first, second), max(first, second)))
 
-dependencies = find_dependencies(town)
-node_with_one_path = find_node_with_only_one_path(dependencies)
+important_street = []
+for first, second in edges:
+    graph[first].remove(second)
+    graph[second].remove(first)
+    visited = [False] * buildings
+    dfs(0, graph, visited)
 
+    if not all(visited):
+        important_street.append((first, second))
 
-nodes_to_remove = []
-while town:
-    if node_with_one_path == None:
-        break
-    value_to_remove = town[node_with_one_path][0]
-    del town[node_with_one_path]
-    for key, values in town.items():
-        for v in values:
-            if v == node_with_one_path:
-                values.remove(v)
-    nodes_to_remove.append((value_to_remove, node_with_one_path))
-    dependencies = find_dependencies(town)
-    node_with_one_path = find_node_with_only_one_path(dependencies)
+    graph[first].append(second)
+    graph[second].append(first)
 
 print("Important streets:")
-for node in sorted(nodes_to_remove):
-    print(f"{node[0]} {node[1]}")
+for first, second in important_street:
+    print(f"{first} {second}")
