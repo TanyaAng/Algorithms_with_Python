68e8f450ce33140ff6dba347e3c2e7d2a7cef6cc
TanyaAng <t.j.angelova@gmail.com>
2022-07-19 20:20:13 +0300

    Lab_exercise

--- 05_graph_theory_exercise/01_areas_in_matrix.py
+++ 05_graph_theory_exercise/01_areas_in_matrix.py
@@ -1,36 +1,79 @@
-def dfs(row, col, matrix, value):
+# Variant 1
+# def dfs(row, col, matrix, value):
+#     if row not in range(0, len(matrix)) or col not in range(0, len(matrix[0])):
+#         return
+#     if matrix[row][col] == '*':
+#         return
+#     if matrix[row][col] != value:
+#         return
+#     matrix[row][col] = '*'
+#     dfs(row + 1, col, matrix, value)
+#     dfs(row - 1, col, matrix, value)
+#     dfs(row, col - 1, matrix, value)
+#     dfs(row, col + 1, matrix, value)
+#
+#
+# n = int(input())
+# m = int(input())
+#
+# matrix = []
+# for _ in range(n):
+#     matrix.append(list(input()))
+# areas = {}
+#
+# for i in range(len(matrix)):
+#     for j in range(len(matrix[0])):
+#         if matrix[i][j] != '*':
+#             row, col = i, j
+#             value = matrix[i][j]
+#             dfs(row, col, matrix, value)
+#             if value not in areas:
+#                 areas[value] = 0
+#             areas[value] += 1
+#
+# print(f"Areas: {sum(areas.values())}")
+#
+# for key, value in sorted(areas.items()):
+#     print(f"Letter '{key}' -> {value}")
+
+# Variant 2
+def dfs(row, col, matrix, visited, key):
     if row not in range(0, len(matrix)) or col not in range(0, len(matrix[0])):
         return
-    if matrix[row][col] == '*':
+    if visited[row][col]:
         return
-    if matrix[row][col] != value:
+    if matrix[row][col] != key:
         return
-    matrix[row][col] = '*'
-    dfs(row + 1, col, matrix, value)
-    dfs(row - 1, col, matrix, value)
-    dfs(row, col - 1, matrix, value)
-    dfs(row, col + 1, matrix, value)
+    visited[row][col] = True
+    dfs(row + 1, col, matrix, visited, key)
+    dfs(row - 1, col, matrix, visited, key)
+    dfs(row, col - 1, matrix, visited, key)
+    dfs(row, col + 1, matrix, visited, key)
 
 
 n = int(input())
 m = int(input())
 
 matrix = []
+visited = []
 for _ in range(n):
     matrix.append(list(input()))
+    visited.append([False] * m)
 areas = {}
+total_areas = 0
 
 for i in range(len(matrix)):
     for j in range(len(matrix[0])):
-        if matrix[i][j] != '*':
-            row, col = i, j
-            value = matrix[i][j]
-            dfs(row, col, matrix, value)
-            if value not in areas:
-                areas[value] = 0
-            areas[value] += 1
+        if visited[i][j]:
+            continue
+        key = matrix[i][j]
+        dfs(i, j, matrix, visited, key)
+        if key not in areas:
+            areas[key] = 0
+        areas[key] += 1
+        total_areas += 1
 
-print(f"Areas: {sum(areas.values())}")
+print(f"Areas: {total_areas}")
 
 for key, value in sorted(areas.items()):
     print(f"Letter '{key}' -> {value}")
--- 05_graph_theory_exercise/02_cycles_in_graph.py
+++ 05_graph_theory_exercise/02_cycles_in_graph.py
@@ -1,20 +1,63 @@
-def find_dependencies(graph):
-    result = {}
-    for node, children in graph.items():
-        if node not in result:
-            result[node] = 0
-        for child in children:
-            if child not in result:
-                result[child] = 1
-            else:
-                result[child] += 1
-    return result
+# def find_dependencies(graph):
+#     result = {}
+#     for node, children in graph.items():
+#         if node not in result:
+#             result[node] = 0
+#         for child in children:
+#             if child not in result:
+#                 result[child] = 1
+#             else:
+#                 result[child] += 1
+#     return result
+#
+#
+# def find_node_without_dependencies(graph):
+#     for node, dependencies in graph.items():
+#         if dependencies == 0:
+#             return node
+#
+#
+# graph = {}
+# while True:
+#     command = input()
+#     if command == 'End':
+#         break
+#     parent, child = command.split('-')
+#     if parent not in graph:
+#         graph[parent] = []
+#     graph[parent].append(child)
+#
+# graph_dependencies = find_dependencies(graph)
+#
+# is_acyclic = True
+# while graph_dependencies:
+#     node_to_remove = find_node_without_dependencies(graph_dependencies)
+#     if node_to_remove == None:
+#         is_acyclic = False
+#         break
+#     graph_dependencies.pop(node_to_remove)
+#     if node_to_remove in graph:
+#         for child in graph[node_to_remove]:
+#             if child in graph_dependencies:
+#                 graph_dependencies[child] -= 1
+#
+# if is_acyclic:
+#     print("Acyclic: Yes")
+# else:
+#     print("Acyclic: No")
 
 
-def find_node_without_dependencies(graph):
-    for node, dependencies in graph.items():
-        if dependencies == 0:
-            return node
+# VARIANT 2
+def dfs(node, graph, visited, cycles):
+    if node in cycles:
+        raise Exception
+    if node in visited:
+        return
+    visited.add(node)
+    cycles.add(node)
+    for child in graph[node]:
+        dfs(child, graph, visited, cycles)
+    cycles.remove(node)
 
 
 graph = {}
@@ -25,23 +68,16 @@ while True:
     parent, child = command.split('-')
     if parent not in graph:
         graph[parent] = []
+    if child not in graph:
+        graph[child] = []
     graph[parent].append(child)
 
-graph_dependencies = find_dependencies(graph)
+visited = set()
 
-is_acyclic = True
-while graph_dependencies:
-    node_to_remove = find_node_without_dependencies(graph_dependencies)
-    if node_to_remove == None:
-        is_acyclic = False
-        break
-    graph_dependencies.pop(node_to_remove)
-    if node_to_remove in graph:
-        for child in graph[node_to_remove]:
-            if child in graph_dependencies:
-                graph_dependencies[child] -= 1
-
-if is_acyclic:
+try:
+    for node in graph:
+        dfs(node, graph, visited, set())
     print("Acyclic: Yes")
-else:
+except Exception:
     print("Acyclic: No")
+
