85dc942e9c0d8b0423b9024a9b29e9c3d1e172c0
TanyaAng <t.j.angelova@gmail.com>
2022-07-31 12:25:09 +0300

    Lab_exercise

--- 07_graph_shortest_path_exercise/05_cable_network.py
+++ 07_graph_shortest_path_exercise/05_cable_network.py
@@ -2,86 +2,56 @@ from queue import PriorityQueue
 
 
 class Edge:
-    def __init__(self, source, destination, weight, connected=False):
+    def __init__(self, source, destination, weight):
         self.source = source
         self.destination = destination
         self.weight = weight
-        self.connected = connected
 
     def __gt__(self, other):
         return self.weight > other.weight
-
-
-def prim(node, graph, forest, forest_edges):
-    forest.add(node)
-    pq = PriorityQueue()
-    for edge in graph[node]:
-        if not edge.connected:
-            pq.put(edge)
-
-    while not pq.empty():
-        min_edge = pq.get()
-        non_tree_node = None
-        if min_edge.source in forest and min_edge.destination not in forest:
-            non_tree_node = min_edge.destination
-
-        elif min_edge.source not in forest and min_edge.destination in forest:
-            non_tree_node = min_edge.source
-        if non_tree_node == None:
-            continue
-
-        forest.add(non_tree_node)
-        forest_edges.append(min_edge)
-        for edge in graph[non_tree_node]:
-            if not edge.connected:
-                pq.put(edge)
 
 
 budget = int(input())
 nodes = int(input())
 edges = int(input())
 
-graph = {}
-visited = set()
-initially_connected = set()
+graph = []
+[graph.append([]) for _ in range(nodes)]
+tree = set()
+
 for _ in range(edges):
     line = input().split()
     source, destination, weight = [int(x) for x in line[0:3]]
-    if len(line) == 4:
-        edge = Edge(source, destination, weight, connected=True)
-        if source not in initially_connected:
-            initially_connected.add(source)
-        if destination not in initially_connected:
-            initially_connected.add(destination)
-    else:
-        edge = Edge(source, destination, weight, connected=False)
-    if source not in graph:
-        graph[source] = []
-    if destination not in graph:
-        graph[destination] = []
-
+    edge = Edge(source, destination, weight)
     graph[source].append(edge)
     graph[destination].append(edge)
+    if len(line) == 4:
+        tree.add(source)
+        tree.add(destination)
 
-forest = set()
-forest_edges = []
-for node in graph:
-    if node in forest:
-        continue
-    prim(node, graph, forest, forest_edges)
+pq = PriorityQueue()
+
+for node in tree:
+    for edge in graph[node]:
+        pq.put(edge)
 
 budget_used = 0
+while not pq.empty():
+    min_edge = pq.get()
+    non_tree_node = None
+    if min_edge.source in tree and min_edge.destination not in tree:
+        non_tree_node = min_edge.destination
+    elif min_edge.source not in tree and min_edge.destination in tree:
+        non_tree_node = min_edge.source
 
-connections = []
-for edge in sorted(forest_edges, key=lambda x: x.weight):
+    if non_tree_node is None:
+        continue
 
-    if edge.source in initially_connected and edge.destination not in initially_connected and budget_used + edge.weight <= budget:
-        initially_connected.add(edge.destination)
-        budget_used += edge.weight
-        connections.append(edge)
-    elif edge.source not in initially_connected and edge.destination in initially_connected and budget_used + edge.weight <= budget:
-        initially_connected.add(edge.source)
-        budget_used += edge.weight
-        connections.append(edge)
+    if budget_used + min_edge.weight > budget:
+        break
+    budget_used += min_edge.weight
+    tree.add(non_tree_node)
+    for edge in graph[non_tree_node]:
+        pq.put(edge)
 
 print(f"Budget used: {budget_used}")
