cfb07bec1509130b41467b3ea6eb268920da7048
TanyaAng <t.j.angelova@gmail.com>
2022-08-02 15:48:55 +0300

    Lab_exercise

--- 09_dynamic_programming/01_binomial_coefficients.py
+++ 09_dynamic_programming/01_binomial_coefficients.py
@@ -2,24 +2,35 @@ n = int(input())
 k = int(input())
 
 
-def factorial(n, memo):
-    if n in memo:
-        return memo[n]
-    if n == 0:
-        memo[0] = 1
-        return memo[0]
-    if n == 1:
-        memo[1] = 1
-        return memo[1]
-    result = n * factorial(n - 1, memo)
-    memo[n] = result
+# VARIANT I
+def calc_binom(n, k, memo):
+    key = f'{n} {k}'
+    if key in memo:
+        return memo[key]
+    if n == 0 or k == 0 or n == k:
+        return 1
+    result = calc_binom(n - 1, k - 1, memo) + calc_binom(n - 1, k, memo)
+    memo[key] = result
     return result
 
+print(calc_binom(n, k, {}))
 
-def find_binomial_coefficient(n, k):
-    memo = {}
-    return factorial(n, memo) / (factorial(k, memo) * factorial(n - k, memo))
+# VARIANT II
+# def factorial(n, memo):
+#     if n in memo:
+#         return memo[n]
+#     if n == 0:
+#         memo[0] = 1
+#         return memo[0]
+#     if n == 1:
+#         memo[1] = 1
+#         return memo[1]
+#     result = n * factorial(n - 1, memo)
+#     memo[n] = result
+#     return result
 
+# def find_binomial_coefficient(n, k):
+#     memo = {}
+#     return factorial(n, memo) / (factorial(k, memo) * factorial(n - k, memo))
 
-print(int(find_binomial_coefficient(n, k)))
-
+# print(int(find_binomial_coefficient(n, k)))
