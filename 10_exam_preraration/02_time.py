39bfcfba6d1ab900cc69bcbd39f023d00f258cbe
TanyaAng <t.j.angelova@gmail.com>
2022-08-04 20:01:19 +0300

    Lab_exercise

--- 10_exam_preraration/02_time.py
+++ 10_exam_preraration/02_time.py
@@ -1,4 +1,5 @@
 from collections import deque
+
 # LONGEST COMMON SUBSEQUENCE PROBLEM
 first = [int(x) for x in input().split()]
 second = [int(x) for x in input().split()]
@@ -9,9 +10,6 @@ cols = len(second) + 1
 dp = []
 [dp.append([0] * cols) for _ in range(rows)]
 
-# for row in dp:
-#     print(row)
-
 for row in range(1, rows):
     for col in range(1, cols):
         if first[row - 1] == second[col - 1]:
@@ -19,21 +17,18 @@ for row in range(1, rows):
         else:
             dp[row][col] = max(dp[row - 1][col], dp[row][col - 1])
 
-# for row in dp:
-#     print(row)
-result=deque()
-row=rows-1
-col=cols-1
-while row>0 and col>0:
-    if first[row-1]==second[col-1]:
-        result.appendleft(first[row-1])
-        row-=1
-        col-=1
-    elif dp[row-1][col]>dp[row][col-1]:
-        row-=1
+result = deque()
+row = rows - 1
+col = cols - 1
+while row > 0 and col > 0:
+    if first[row - 1] == second[col - 1]:
+        result.appendleft(first[row - 1])
+        row -= 1
+        col -= 1
+    elif dp[row - 1][col] > dp[row][col - 1]:
+        row -= 1
     else:
-        col-=1
+        col -= 1
 
 print(*result, sep=' ')
 print(dp[rows - 1][cols - 1])
-
