83f75c0356f6b1de212adbe49cb332587106f623
TanyaAng <t.j.angelova@gmail.com>
2022-08-04 18:23:58 +0300

    Lab_exercise

--- exam_preparation/23_jan_2022/02_universes.py
+++ exam_preparation/23_jan_2022/02_universes.py
@@ -6,30 +6,26 @@ def find_root(parent, node):
 
 lines = int(input())
 
-graph = {}
+nodes = set()
 edges = []
 
 for _ in range(lines):
     first, second = input().split()
     edge = (first, second)
-    edges.append(edge)
-    if first not in graph:
-        graph[first] = []
-    graph[first].append(second)
-    if second not in graph:
-        graph[second] = []
-    graph[second].append(first)
+    edges.append((first,second))
+    nodes.add(first)
+    nodes.add(second)
 
-print(edges)
-parent = {node: node for node in graph}
-print(parent)
+# print(edges)
+parent = {node: node for node in nodes}
+# print(parent)
 for edge in edges:
     first = find_root(parent, edge[0])
     second = find_root(parent, edge[1])
     if first != second:
         parent[first] = second
 
-print(parent)
+# print(parent)
 counter = 0
 for key, value in parent.items():
     if key == value:
