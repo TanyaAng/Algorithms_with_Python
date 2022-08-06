12078c15092bb940dbc6d62375f026128579eeb2
TanyaAng <t.j.angelova@gmail.com>
2022-08-06 10:39:51 +0300

    Lab_exercise

--- exam_preparation/24_jul_2022/01_trains.py
+++ exam_preparation/24_jul_2022/01_trains.py
@@ -1,2 +1,23 @@
-arrivals = [int(x) for x in input().split()]
-departures = [int(x) for x in input().split()]
+arrivals = sorted([float(x) for x in input().split()])
+departures = sorted([float(x) for x in input().split()])
+print(arrivals)
+print(departures)
+lenght = len(arrivals)
+
+result = 1
+platform_needed = 1
+i = 1
+j = 0
+
+while i < lenght and j < lenght:
+    if arrivals[i] < departures[j]:
+        platform_needed += 1
+        i += 1
+
+    elif arrivals[i] >= departures[j]:
+        platform_needed -= 1
+        j += 1
+
+    result = max(result, platform_needed)
+
+print(result)
